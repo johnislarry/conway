3799a1ecf64fe1fea5f1b99405ca98591c0da828
Max Sherman <john@maxsherman.com>
2015-02-17 02:05:34 -0800

    gitignore and start/stop button

--- conway.js
+++ conway.js
@@ -5,6 +5,42 @@ var WIDTH = 10;
 var SIZE = 40;
 var DEAD = "white";
 var ALIVE = "black";
+var TIME = 100;
+
+var isLoopRunning = false;
+var loopID;
+var board;
+var nodeBoard;
+
+function main() {
+  var table = document.createElement("table");
+  table.style.border = "1px solid #000";
+  document.body.appendChild(table);
+  var button = makeButton();
+  var init = drawInitialBoard(table);
+  board = init.b;
+  nodeBoard = init.nb;
+}
+
+function makeButton() {
+  var button = document.createElement("button");
+  button.style.width = "100px";
+  button.style.height = "50px";
+  button.onclick = function() {
+    if (isLoopRunning) {
+      clearInterval(loopID);
+    } else {
+      loopID = setInterval(function() {
+        board = getUpdatedBoard(board);
+        drawBoard(board, nodeBoard);
+      }, TIME);
+    }
+    isLoopRunning = !isLoopRunning;
+  };
+  button.innerHTML = "start/stop";
+  document.body.appendChild(button);
+  return button;
+}
 
 function drawInitialBoard(table) {
   var board = [];
@@ -16,7 +52,6 @@ function drawInitialBoard(table) {
 
     board[i] = [];
     nodeBoard[i] = [];
-
     for (var j = 0; j < WIDTH; j++) {
       var state = DEAD;
       if (i == 2 && j == 0 || i == 2 && j == 1 || i == 2 && j == 2
@@ -27,7 +62,6 @@ function drawInitialBoard(table) {
       td.style.width = SIZE + "px";
       td.style.height = SIZE + "px";
       td.style.background = state;
-
       tr.appendChild(td);
 
       board[i][j] = state;
@@ -36,22 +70,6 @@ function drawInitialBoard(table) {
   }
   return {b:board, nb:nodeBoard};
 }
-
-function main() {
-  var table = document.createElement("table");
-  table.style.border = "1px solid #000";
-  document.body.appendChild(table);
-  var init = drawInitialBoard(table);
-  var board = init.b;
-  var nodeBoard = init.nb;
-  setInterval(function() {
-    board = getUpdatedBoard(board);
-    drawBoard(board, nodeBoard);
-  },
-  100
-  );
-}
-
 
 function getUpdatedBoard(oldBoard) {
   var newBoard = [];
@@ -70,16 +88,17 @@ function getUpdatedBoard(oldBoard) {
           }
         }
       }
+
       if (oldBoard[i][j] == DEAD && aliveCount == 3) {
         newBoard[i][j] = ALIVE;
       } else if (oldBoard[i][j] == ALIVE) {
         if (aliveCount < 2 || aliveCount > 3) {
           newBoard[i][j] = DEAD;
         } else {
-          newBoard[i][j] = ALIVE;//oldBoard[i][j];
+          newBoard[i][j] = ALIVE;
         }
       } else {
-        newBoard[i][j] = DEAD;//oldBoard[i][j];
+        newBoard[i][j] = DEAD;
       }
     }
   }
